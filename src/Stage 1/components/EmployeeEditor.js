6abce221be4d57efa14678b6e9de29469194bac2
Scott Serage <xttocsurge51@gmail.com>
2017-09-11 14:29:27 -0600

    This time actually Completed Stage 1

--- src/Stage 1/components/EmployeeEditor.js
+++ src/Stage 1/components/EmployeeEditor.js
@@ -8,6 +8,8 @@ class EmployeeEditor extends Component {
       originalEmployee: null,
       notModified: true
     };
+    this.save = this.save.bind(this);
+    this.cancel = this.cancel.bind(this);
   }
 
   componentWillReceiveProps(props) {
@@ -36,13 +38,13 @@ class EmployeeEditor extends Component {
     var employeeCopy = Object.assign({}, this.state.originalEmployee);
     this.setState({ employee: employeeCopy, notModified: true });
   }
-  
+
   render() {
     return (
       <div className="infoCard">
-        { 
+        {
           this.state.employee
-          ? 
+          ?
           <div>
             <span id="employeeID"> ID: { this.state.employee.id } </span>
             <p id="employeeTitle"> { this.state.originalEmployee.name } </p>
@@ -60,7 +62,7 @@ class EmployeeEditor extends Component {
           :
           <p id="noEmployee"> No Employee Selected </p>
         }
-       
+
       </div>
     )
   }
