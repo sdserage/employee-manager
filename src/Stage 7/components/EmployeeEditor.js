97fad6489ce76df1a4da22aa2e846ed0d87c30ab
Scott Serage <xttocsurge51@gmail.com>
2017-09-11 17:08:42 -0600

    Completed Stage 7

--- src/Stage 7/components/EmployeeEditor.js
+++ src/Stage 7/components/EmployeeEditor.js
@@ -1,22 +1,53 @@
 import React, { Component } from 'react';
 
 class EmployeeEditor extends Component {
-  // constructor
+  constructor(){
+    super();
+    this.state = {
+      employee:null,
+      originalEmployee:null,
+      notModified:true
+    }
+    this.save = this.save.bind(this);
+    this.cancel = this.cancel.bind(this);
+  }
 
-  // componentWillReceiveProps
+  componentWillReceiveProps(props){
+    this.setState({
+      employee:Object.assign({},props.selected),
+      originalEmployee:props.selected,
+      notModified:true
+    })
+  }
 
-  // handleChange
+  handleChange(prop,val){
+    if(this.state.notModified){
+      this.setState({notModified:false});
+    }
+    let employeeCopy = Object.assign({},this.state.employee);
+    employeeCopy[prop] = val;
+    this.setState({employee:employeeCopy});
+  }
 
-  // save
+  save(){
+    this.state.originalEmployee.updateName(this.state.employee.name);
+    this.state.originalEmployee.updatePhone(this.state.employee.phone);
+    this.state.originalEmployee.updateTitle(this.state.employee.title);
+    this.setState({notModified:true});
+    this.props.refreshList();
+  }
 
-  // cancel
-  
+  cancel(){
+    let employeeCopy=Object.assign({},this.state.originalEmployee);
+    this.setState({employee:employeeCopy,notModified:true});
+  }
+
   render() {
     return (
       <div className="infoCard">
-        { 
+        {
           this.state.employee
-          ? 
+          ?
           <div>
             <span id="employeeID"> ID: { this.state.employee.id } </span>
             <p id="employeeTitle"> { this.state.originalEmployee.name } </p>
@@ -34,7 +65,7 @@ class EmployeeEditor extends Component {
           :
           <p id="noEmployee"> No Employee Selected </p>
         }
-       
+
       </div>
     )
   }
