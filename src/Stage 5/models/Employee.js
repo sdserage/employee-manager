a0e4fa8ea87736ea547b54259c138b6c84fd6e58
Scott Serage <xttocsurge51@gmail.com>
2017-09-11 15:46:07 -0600

    Completed Stage 5

--- src/Stage 5/models/Employee.js
+++ src/Stage 5/models/Employee.js
@@ -1,9 +1,11 @@
 export default class Employee {
-  // constructor
-  
-  // updateName
-
-  // updatePhone
-
-   // updateTitle
+  constructor(id,name,phone,title){
+    this.id = id;
+    this.name = name;
+    this.phone = phone;
+    this.title = title;
+  }
+  updateName(newName){this.name=newName};
+  updatePhone(newPhone){this.phone=newPhone};
+  updateTitle(newTitle){this.title=newTitle};
 }
